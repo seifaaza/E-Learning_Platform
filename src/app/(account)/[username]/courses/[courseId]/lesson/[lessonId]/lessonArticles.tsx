b0c35d7810acbf9395ffd4607c9c65a8b2dc40eb
seif <seifeddine.aaza@gmail.com>
2024-09-01 02:25:56 +0100

    update

--- src/app/(account)/[username]/courses/[courseId]/lesson/[lessonId]/lessonArticles.tsx
+++ src/app/(account)/[username]/courses/[courseId]/lesson/[lessonId]/lessonArticles.tsx
@@ -14,11 +14,11 @@ interface LessonArticlesProps {
 
 const LessonArticles: React.FC<LessonArticlesProps> = ({ articles }) => {
   return (
-    <ScrollArea className="h-60 lg:h-72 xl:h-96 w-full bg-main/5 p-2 xl:p-4 rounded-lg">
-      <ul className="flex flex-col gap-3 xl:gap-4">
+    <ScrollArea className="h-60 lg:h-72 xl:h-96 w-full bg-main/5 p-3 xl:p-4 rounded-lg">
+      <ul className="flex flex-col gap-4">
         {articles.map((article, index) => (
           <li key={article._id}>
-            <h6 className="text-gray-900 font-medium mb-1">
+            <h6 className="text-gray-900 font-medium mb-2">
               {index + 1}, {article.title}
             </h6>
             <h6 className="text-gray-700">{article.content}</h6>
